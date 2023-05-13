6e82a4f3c72143ccc46e69b062c559a045d09213
dhairyaamdaan <132569224+dhairyaamdaan@users.noreply.github.com>
2023-05-13 22:47:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,14 +7,14 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
+API_ID = int(os.getenv("API_ID", "25757134"))
+API_HASH = os.environ.get("API_HASH", "4ab2cea7e4d196df9c850ac7f1c91c7f")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5959236310:AAGSD6oUi0qdZQ4HknQTfmfSA9QQQzw_SEI")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5483441304")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
 DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+OWNER_ID =  int(os.environ.get("OWNER_ID", 5483441304")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(Id Owned Id)
 #  Optionnal variables
